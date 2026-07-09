8da7a41a663f2f9e17525ce76a3c07ec2a3f3831
lavanya-sharma28 <ggunnu8866@gmail.com>
2026-07-09 14:31:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const decbtn = document.getElementById("decbtn");
+const rstbtn = document.getElementById("rstbtn");
+const incbtn = document.getElementById("incbtn");
+const countlabel = document.getElementById("countlabel");
+let count= 0;
+incbtn.onclick= function(){
+    count ++;
+    countlabel.textContent=count;
+}
+decbtn.onclick= function(){
+    count--;
+    countlabel.textContent=count;
+}
+rstbtn.onclick=function(){
+    count=0;
+    countlabel.textContent=count;
+}
